ee8c13adf8a2d1eea11fea33608e8553e56470c4
jiangweiquant <3202263365@qq.com>
2023-07-10 02:12:47 +0800

    Create 4k图片解析下载.py

---
+++ 爬虫/第二章requests模块基础/xpath解析基础/4k图片解析下载/4k图片解析下载.py
@@ -0,0 +1,18 @@
+import requests
+from lxml import etree
+
+#第一步从网站中爬取页面源码数据
+url = 'http://pic.netbian.com/4kdongwu/'
+header = {'User_Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'}
+page_text = requests.get(url=url, headers=header).text
+with open('4k_图片.html', 'w', encoding='utf-8') as fp:
+    fp.write(page_text)
+# #数据解析
+# tree = etree.HTML(page_text)
+# div_list = tree.xpath('//section[@class="list"]/div')
+# print(div_list)
+# fp = open('58.txt','w',encoding='utf-8')
+# for div in div_list:
+#     title = div.xpath('.//h3[@class="property-content-title-name"]/text()')[0]
+#     print(title)
+#     fp.write(title+'\n')
