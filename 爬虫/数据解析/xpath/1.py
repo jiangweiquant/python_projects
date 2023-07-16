7fd0365e6921a4697b8d73aba9c303e999d0dca1
jiangweiquant <3202263365@qq.com>
2023-07-17 03:08:45 +0800

    Update 1.py

--- 爬虫/数据解析/xpath/1.py
+++ 爬虫/数据解析/xpath/1.py
@@ -1,11 +1,11 @@
 from lxml import etree
 
 # 实例一个etree对象，将解析的内容加载到该对象中
-tree = etree.parse('test.html', etree.HTMLParser())
+tree = etree.XML('test.html')
 # r = tree.xpath('/html/body/ul/li')
 # r = tree.xpath('/html//li')
 # r = tree.xpath('//li')
 # r = tree.xpath('//div[@class="song"]')
 # r = tree.xpath('//div[@class="song"]//text()')
-r = tree.xpath('//img/@src')
+r = tree.xpath('//img/@src')#提取属性值
 print(r)
